68a412814d02a73b24482cd652b58a8ef3a4da8e
Edward Clark <eeclark@gmail.com>
2015-01-22 22:34:01 -0600

    added struct

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,14 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Edward Clark on 1/22/15.
+//  Copyright (c) 2015 Edward Clark. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var image = UIImage(named:"default.jpg")
+}
--- 99RedBalloons/ViewController.swift
+++ 99RedBalloons/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balloonLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    
+    var myBalloons:[Balloon] = []
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        var firstBalloon = Balloon()
+        firstBalloon.image = UIImage(named:"RedBalloon1.jpg")
+        
+        var secondBalloon = Balloon()
+        firstBalloon.image = UIImage(named:"RedBalloon2.jpg")
+        
+        var thirdBalloon = Balloon()
+        firstBalloon.image = UIImage(named:"RedBalloon3.jpg")
+        
+        var fourthBalloon = Balloon()
+        firstBalloon.image = UIImage(named:"RedBalloon4.jpg")
+        
+        self.myBalloons += [firstBalloon,secondBalloon,thirdBalloon,fourthBalloon]
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonPressed(sender: UIBarButtonItem) {
+        
+        var randomIndex:Int
+        do {
+            randomIndex = Int(arc4random_uniform(UInt32(self.myBalloons.count)))
+        } while self.currentIndex == randomIndex
+        
+        self.currentIndex == randomIndex
+        
+        let balloon = self.myBalloons[randomIndex]
+        
+        self.balloonLabel.text = "\(currentIndex)" + "balloon"
+        self.myImageView.image = balloon.image
+    }
 
 }
 
